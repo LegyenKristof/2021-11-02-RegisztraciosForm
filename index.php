44663999922815e6db4aaa038b3b040b085eaedf
Your Name <you@example.com>
2021-11-02 14:40:52 +0100

    Kinézet kész és inicializálás

--- index.php
+++ index.php
@@ -5,24 +5,26 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <link rel="stylesheet" href="main.css">
+    <script src="main.js"></script>
 </head>
 <body>
 
 <form>
     <div>
-        <span>Név</span>
+        <span>Név:</span>
         <input type="text" name="nev" id="nev">
         <span id="nevSzamlalo">0 / 20</span>
     </div>
     <div>
-        <span>Jelszó</span>
-        <input type="text" name="jelszo" id="jelszo">
+        <span>Jelszó:</span>
+        <input type="password" name="jelszo" id="jelszo">
         <span id="jelszoSzamlalo">0 / 8</span>
     </div>
     <div>
-        <span>Jelszó ismét</span>
-        <input type="text" name="jelszoIsmet" id="jelszoIsmet">
-        <span id="jelszoIsmetSzamlalo"></span>
+        <span>Jelszó ismét:</span>
+        <input type="password" name="jelszoIsmet" id="jelszoIsmet">
+        <span id="jelszoIsmetHelyes"></span>
     </div>
     <div>
         <input type="submit" value="Regisztráció">
---
+++ main.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", init)
+let nev, nevHossz, jelszo, jelszoHossz, jelszoIsmet, jelszoIsmetHelyes;
+
+function init(){
+    nev = document.getElementById("nev");
+    nevHossz = document.getElementById("nevHossz");
+    jelszo = document.getElementById("jelszo");
+    jelszoHossz = document.getElementById("jelszoHossz");
+    jelszoIsmet = document.getElementById("jelszoIsmet");
+    jelszoIsmetHelyes = document.getElementById("jelszoIsmetHelyes");
+
+    
+}
