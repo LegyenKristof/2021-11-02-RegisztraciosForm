d45d57b8e93180e7d14e02a8ceb1c26a34981bd7
Your Name <you@example.com>
2021-11-02 14:19:45 +0100

    Első commit

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+<form>
+    <div>
+        <span>Név</span>
+        <input type="text" name="nev" id="nev">
+        <span id="nevSzamlalo">0 / 20</span>
+    </div>
+    <div>
+        <span>Jelszó</span>
+        <input type="text" name="jelszo" id="jelszo">
+        <span id="jelszoSzamlalo">0 / 8</span>
+    </div>
+    <div>
+        <span>Jelszó ismét</span>
+        <input type="text" name="jelszoIsmet" id="jelszoIsmet">
+        <span id="jelszoIsmetSzamlalo"></span>
+    </div>
+    <div>
+        <input type="submit" value="Regisztráció">
+    </div>
+</form>
+    
+</body>
+</html>
